64945797773743d3cfe60a26db104372691ea046
yeasinarafatprantik2002 <yeasinarafatprantik@gmail.com>
2024-08-22 00:43:05 +0600

    chore: Fix typo in cookieParser import and rename register function in user.controller.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import cockieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cockieParser());
+app.use(cookieParser());
 
 // import routes
 import userRouter from "./routes/user.route.js";
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { register } from "../controllers/user.controller.js";
+import { registerUser, loginUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.route("/register").post(upload.single("avatar"), register);
+router.route("/register").post(upload.single("avatar"), registerUser);
+router.route("/login").post(loginUser);
 
 export default router;
